4ddfae6237566b436e5f105540730e16aba92b70
Vulpes107 <wukp123@gmail.com>
2024-12-06 19:38:51 +0100

    configs

--- src/dashboard/frontend/src/app/webSocket/web-socket.service.ts
+++ src/dashboard/frontend/src/app/webSocket/web-socket.service.ts
@@ -60,7 +60,7 @@ export class WebSocketService {
   
  constructor() {
     this.webSocket = new Socket({
-    url: "http://10.1.207.129:5005",
+    url: "http://10.1.207.142:5005",
     options: {},
     });
 
