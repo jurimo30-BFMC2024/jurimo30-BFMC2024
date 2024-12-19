145a27aa7818759eec49dc413f9bf7e0d8465dd8
bagi002 <blagojemilosevic123@gmail.com>
2024-12-19 15:23:45 +0100

    subscribing core to dash

--- src/core/Manual/manualControlMode.py
+++ src/core/Manual/manualControlMode.py
@@ -2,6 +2,13 @@ from src.utils.messages.allMessages import (mainCamera)
 from src.utils.messages.messageHandlerSubscriber import messageHandlerSubscriber
 from src.utils.messages.messageHandlerSender import messageHandlerSender
 
+from src.utils.messages.allMessages import (
+    Control,
+    SteerMotor,
+    SpeedMotor,
+    Brake
+)
+
 class manualControlMode():
     def __init__(self, queueList, logging, debugging=False):
         self.queuesList = queueList
@@ -10,8 +17,17 @@ class manualControlMode():
         self.subscribe()
 
     def subscribe(self):
-        """Subscribes to the messages you are interested in"""
-        pass
+        self.controlSubscriber = messageHandlerSubscriber(self.queuesList, Control, "lastOnly", True)
+        self.steerMotorSubscriber = messageHandlerSubscriber(self.queuesList, SteerMotor, "lastOnly", True)
+        self.speedMotorSubscriber = messageHandlerSubscriber(self.queuesList, SpeedMotor, "lastOnly", True)
+        self.brakeSubscriber = messageHandlerSubscriber(self.queuesList, Brake, "lastOnly", True)
 
     def run(self):
-        pass
+        if(self.speedMotorSubscriber.isDataInPipe):
+            speedRecv = self.speedMotorSubscriber.receive
+        if(self.steerMotorSubscriber.isDataInPipe):
+            steerRecv = self.steerMotorSubscriber.receive
+        if(self.brakeSubscriber.isDataInPipe):
+            speedRecv = self.brakeSubscriber.receive
+        if(self.controlSubscriber.isDataInPipe):
+            speedRecv = self.controlSubscriber.receive
