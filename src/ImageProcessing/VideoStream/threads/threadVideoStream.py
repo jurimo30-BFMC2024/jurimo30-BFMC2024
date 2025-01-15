572f6ea10d3ae4fd4b2d771e8ff4b006f85af564
Vulpes107 <wukp123@gmail.com>
2025-01-15 01:34:54 +0100

    Implement an example for serial and main camera

--- src/ImageProcessing/VideoStream/threads/threadVideoStream.py
+++ src/ImageProcessing/VideoStream/threads/threadVideoStream.py
@@ -1,9 +1,14 @@
 from src.templates.threadwithstop import ThreadWithStop
-from src.utils.messages.allMessages import (serialCamera)
+from src.utils.messages.allMessages import (serialCamera, mainCamera)
 from src.utils.messages.messageHandlerSubscriber import messageHandlerSubscriber
 from src.utils.messages.messageHandlerSender import messageHandlerSender
 from src.ImageProcessing.VideoStream.VideoGridStreamer import VideoGridStreamer
 from threading import Thread
+from multiprocessing import Process
+import numpy as np
+import cv2
+import base64
+import time
 
 class threadVideoStream(ThreadWithStop):
     """This thread handles VideoStream.
@@ -20,11 +25,39 @@ class threadVideoStream(ThreadWithStop):
         self.debugging = debugging
         self.subscribe()
         super(threadVideoStream, self).__init__()
+        self.displayThreads = [
+            Thread(target=self.displayRawCamera, daemon=True, kwargs={
+                "subscriber": messageHandlerSubscriber(self.queuesList, serialCamera, "lastOnly", True), 
+                "row": 0,
+                "col": 0,
+            }),
+            Thread(target=self.displayRawCamera, daemon=True, kwargs={
+                "subscriber": messageHandlerSubscriber(self.queuesList, mainCamera, "lastOnly", True), 
+                "row": 1,
+                "col": 0,
+            })
+        ]
 
-        self.cameraSubscribers
+    @staticmethod
+    def decode_frame(encoded_data):
+        """Decode base64 encoded frame to an OpenCV image."""
+        frame_data = base64.b64decode(encoded_data)
+        np_array = np.frombuffer(frame_data, np.uint8)
+        frame = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+        return frame
+    
+    def displayRawCamera(self, subscriber: messageHandlerSubscriber, row: int, col: int):
+        while self._running:
+            videoData = subscriber.receiveWithBlock()
+
+            frame = self.decode_frame(videoData)
+
+            self.streamer.display_frame(frame, row=row, col=col)
+            # time.sleep(0.05)
 
     def run(self):
-        pass
+        for th in self.displayThreads:
+            th.start()
 
     def subscribe(self):
         """Subscribes to the messages you are interested in"""
