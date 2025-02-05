96a9203af3cc2ce466ae1e3b4a6ff335ae1c8caa
Vulpes107 <wukp123@gmail.com>
2025-02-05 14:16:39 +0100

    add parking logic

--- src/core/Auto/Parking/Parking.py
+++ src/core/Auto/Parking/Parking.py
@@ -12,3 +12,59 @@ class Parking():
         self.queuesList = queueList
         self.logging = logging
         self.debugging = debugging
+
+        self.state = "finish"
+        self.angle = 0
+        self.speed = 0
+
+        self.left_spot_taken = False
+        self.right_spot_taken = False
+
+
+    def run(self, parkingSpotDetected, sideSensors):
+
+        if self.state == "finish":
+            self.state = "search_parking_spot"
+            self.angle, self.speed = 0, 0
+
+        elif self.state == "search_parking_spot":
+            if parkingSpotDetected is not None:
+                self.left_spot_taken = False
+                self.right_spot_taken = False
+                self.state = "search_and_evaluate"
+
+        elif self.state == "search_and_evaluate":
+            if self.evaluate_side_sensors():
+                self.state = "search_parking_spot"
+            elif parkingSpotDetected is not None:
+                self.state = "park"
+
+        elif self.state == "park":
+            self.perform_parking()
+            self.state = "unpark"
+
+        elif self.state == "unpark":
+            self.perform_unparking()
+            self.state = "finish"
+
+        self.logging.debug(f"Current state: {self.state}")
+
+        return self.angle, self.speed, self.state != "finish"
+
+    def evaluate_side_sensors(self, sideSensors):
+        # Implement logic to evaluate side sensors
+        left_spot = sideSensors["left"] != 0
+        right_spot = sideSensors["right"] != 0
+
+        self.left_spot_taken = self.left_spot_taken or left_spot
+        self.right_spot_taken = self.right_spot_taken or right_spot
+
+        return self.left_spot_taken and self.right_spot_taken
+
+    def perform_parking(self):
+        self.logging.info("Performing parking maneuver...")
+        # Implement parking logic
+
+    def perform_unparking(self):
+        self.logging.info("Performing unparking maneuver...")
+        # Implement unparking logic
