46772cc87a62ddcdd6d91b5eb2fd174b22f5d76d
resetarp <petar03resetar@gmail.com>
2025-06-23 21:03:05 +0300

    - adjust path for semi-finals

--- src/core/Auto/pathPlanning/pathPlanning.py
+++ src/core/Auto/pathPlanning/pathPlanning.py
@@ -23,14 +23,15 @@ class PathPlanner:
            
         self.roundabout_entries = ["317", "367", "397", "405"]
         self.roundabout_exits = ["368", "342", "398", "318"]
-        self.hardInstructionQueue =  ["Right", "Straight", "Right", "Exit 1", "Exit 4", "Exit 3", "Right",
-                                  "Right","Straight", "Straight", "Left", "Straight", "Straight","Straight",
-                                  "Left","Left", "Left", "Right", "Straight", "Right", "Left", "Left",
-                                  "Right", "Straight", "Exit 4", "Straight"]
+        self.hardInstructionQueue = ["Right", "Straight", "Right", "Exit 1", "Exit 4", "Exit 3", "Right", "Right",
+                            "Straight", "Straight", "Left", "Straight", "Straight", "Straight", "Left",
+                            "Left", "Left", "Right", "Straight", "Right", "Straight", "Left", "Straight", "Right",
+                            "Exit 4", "Straight", "Straight", "Left", "Left", "Left", "Right", "END_OF_PATH"]
         
         self.localizationPath = [("223", "243"), ("246","54"), ("55", "317"), ("368", "397"), ("343", "367"), ("318", "56"), ("49", "288"), ("302", "6"),
                                       ("1", "18"), ("13", "91"), ("88", "102"), ("97", "75"), ("70", "185"), ("188", "191"), ("193", "198"), ("201", "42"), ("39", "206"),
-                                      ("208", "71"), ("74", "98"), ("99", "26"), ("31", "16"), ("13", "91"), ("92", "80"), ("83", "404"), ("399", "84"), ("79", "93")]
+                                      ("208", "71"), ("74", "98"), ("99", "26"), ("29", "38"), ("43", "4"), ("7", "82"), ("83", "404"), ("399", "84"), ("79", "93"), 
+                                      ("88", "102"), ("99", "26"), ("31", "16"), ("13", "91"), ("92", "80")]
 
     def planPath(self):
         '''Generates a queue of instructions'''
