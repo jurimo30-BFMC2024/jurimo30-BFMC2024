dbec263123cabb2458c065c28c83623231a701b8
Petar Rešetar <petar03resetar@gmail.com>
2025-06-25 10:47:59 +0200

    - add arena path (#138)

--- src/core/Auto/pathPlanning/pathPlanning.py
+++ src/core/Auto/pathPlanning/pathPlanning.py
@@ -27,18 +27,28 @@ class PathPlanner:
                                   "Right","Straight", "Straight", "Left", "Straight", "Straight","Straight",
                                   "Left","Left", "Left", "Right", "Straight", "Right", "Left", "Left",
                                   "Right", "Straight", "Exit 4", "Straight"]
-        
+
         self.localizationPath = [("223", "243"), ("246","54"), ("55", "317"), ("368", "397"), ("343", "367"), ("318", "56"), ("49", "288"), ("302", "6"),
-                                      ("1", "18"), ("13", "91"), ("88", "102"), ("97", "75"), ("70", "185"), ("188", "191"), ("193", "198"), ("201", "42"), ("39", "206"),
-                                      ("208", "71"), ("74", "98"), ("99", "26"), ("31", "16"), ("13", "91"), ("92", "80"), ("83", "404"), ("399", "84"), ("79", "93")]
+                                 ("1", "18"), ("13", "91"), ("88", "102"), ("97", "75"), ("70", "185"), ("188", "191"), ("193", "198"), ("201", "42"), ("39", "206"),
+                                 ("208", "71"), ("74", "98"), ("99", "26"), ("31", "16"), ("13", "91"), ("92", "80"), ("83", "404"), ("399", "84"), ("79", "93")]
+        
+        # for arena
+        # self.hardInstructionQueue = ["Left", "Right", "Right", "Left", "Straight", "Straight", "Right", "Straight", "Right", "Exit 1", "Exit 1"]
+        
+        # self.localizationPath = [("183", "93"), ("90", "14"), ("15", "32"), ("25", "100"), ("97", "75"), ("70", "185"), ("188", "191"), ("223", "243"), 
+        #                          ("246","54"), ("55", "317"),  ("368", "397"), ("399", "400")]
+        
+
 
     def planPath(self):
         '''Generates a queue of instructions'''
         graph = self.parse_graphml(self.file_path)
-        best_path = self.find_greedy_path(graph, self.start, "191")
+        instructionQueue = []
         segments = []
+        # OVO ZAKOMENTARISATI KAD SE BUDE RADILA ARENA
+        ###########################################################
+        best_path = self.find_greedy_path(graph, self.start, "191")
         if best_path:
-            instructionQueue = []
             turns = self.determine_turns(graph, best_path)
             for node, direction in turns:
                 instructionQueue.append(direction)
@@ -47,7 +57,7 @@ class PathPlanner:
                 segment = self.calculate_path_segments(graph, segment_path)
                 if segment:
                     segments.append(segment)
-            
+        ###########################################################    
             # add hardcoded path
             instructionQueue.extend(self.hardInstructionQueue)
 
